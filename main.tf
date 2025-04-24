a69385c3c30e4789051673fb39e4d861dc88e5e1
Devron Tombacco <tombacco.devron@gmail.com>
2025-04-24 09:08:11 +0100

    Add internet Gateway

--- main.tf
+++ main.tf
@@ -24,3 +24,11 @@ resource "aws_vpc" "main_vpc" {
             Name = "main_vpc"
         }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "igw"
+  }
+}
